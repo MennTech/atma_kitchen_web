424846bbde2dc0b012d658628650997ec45863e8
dankski1337 <simonagis23@gmail.com>
2024-04-27 02:59:46 +0700

    init projek minggu 1 (auth api, components, login pages, routes, layouts)

---
+++ src/api/index.jsx
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+export const BASE_URL = "http://127.0.0.1:8000"; // ganti kalo servernya dah dihosting
+
+export const getProdukPhoto = (photo) => {
+    return `${BASE_URL}/storage/produk/${photo}`
+}
+
+const useAxios = axios.create({
+    baseURL: `${BASE_URL}/api/`
+})
+
+export default useAxios;
