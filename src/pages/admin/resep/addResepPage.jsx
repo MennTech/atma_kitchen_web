3225386869ffd4f5cd95b7698f0ed8b3286d511e
MennTech <memen999@gmail.com>
2024-05-02 00:50:37 +0700

    add resep jadi

--- src/pages/admin/resep/addResepPage.jsx
+++ src/pages/admin/resep/addResepPage.jsx
@@ -2,48 +2,55 @@ import { useState, useEffect } from "react";
 import { GetAllBahanBaku } from "../../../api/BahanBaku";
 import { TambahResep } from "../../../api/resepApi";
 import { useNavigate } from "react-router-dom";
+import { toast } from 'sonner';
 const AddResepPage = () => {
   const navigate = useNavigate();
   const [dataBahanBaku, setDataBahanBaku] = useState([]);
   const [namaResep, setNamaResep] = useState("");
-  const [detailResep, setDetailResep] = useState([
-    {
-      id_bahan_baku: "",
-      jumlah_bahan: 0,
-    },
-  ]);
+  const [detailResep, setDetailResep] = useState([]);
+  const [errors, setErrors] = useState({});
 
   const fecthDataBahanBaku = () => {
     GetAllBahanBaku()
       .then((response) => {
         setDataBahanBaku(response);
-        console.log(response);
       })
       .catch((error) => {
         console.log(error);
       });
   };
+
   useEffect(() => {
     fecthDataBahanBaku();
+    setDetailResep([{ id_bahan_baku: "", jumlah_bahan: 0 }]);
   }, []);
 
   const handleNamaResep = (event) => {
     setNamaResep(event.target.value);
   };
 
-  const addResep = () => {
-    const data = {
-      nama_resep: namaResep,
-      detail_resep: detailResep,
-    };
-    TambahResep(data)
-      .then((response) => {
-        navigate("/resep"); 
-        console.log(response);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+  const addResep = (event) => {
+    event.preventDefault();
+    const validationErrors = {};
+    if (!namaResep.trim()) {
+      validationErrors.nama_resep = "Nama Resep harus diisi";
+      toast.error("Nama Resep harus diisi");
+    }
+    setErrors(validationErrors);
+    if (Object.keys(validationErrors).length === 0) {
+      const data = {
+        nama_resep: namaResep,
+        detail_resep: detailResep,
+      };
+      TambahResep(data)
+        .then((response) => {
+          toast.success("Resep berhasil ditambahkan");
+          navigate("/dashboard/resep");
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
   };
 
   const handleClick = (event) => {
@@ -65,25 +72,26 @@ const AddResepPage = () => {
   };
 
   return (
-    <div className="w-screen">
-      <div className="flex justify-between mt-5 ms-3 me-3">
-        <h1 className="text-3xl text-[#d08854] font-semibold">Tambah Resep</h1>
+    <div className="w-screen p-4 min-h-screen overflow-y-auto">
+      <div className="flex items-center">
+        <h1 className="text-4xl text-[#d08854] font-semibold">Tambah Resep</h1>
+        <div className="divider divider-horizontal m-1"></div>
+        <p className="text-slate-400">Manajemen Resep Atma Kitchen</p>
       </div>
-      <div className="mt-5 mx-3">
-        <form onSubmit={addResep}>
-          <div className="form-control">
-            <label htmlFor="namaResep">Nama Resep</label>
-            <div className="grid grid-cols-2">
-              <div>
-                <input
-                  type="text"
-                  id="namaResep"
-                  name="nama_resep"
-                  className="input input-bordered bg-white"
-                  onChange={handleNamaResep}
-                />
+
+      <div className="card w-full h-fit bg-white mt-4">
+        <div className="card-body h-full p-4">
+          <form onSubmit={addResep}>
+            <div className="flex justify-between">
+              <div className="flex items-center">
+                <button
+                  className="btn btn-error text-white"
+                  onClick={() => navigate("/dashboard/resep")}
+                >
+                  Cancel
+                </button>
               </div>
-              <div>
+              <div className="space-x-1">
                 <button
                   className="btn btn-primary text-white"
                   onClick={handleClick}
@@ -92,61 +100,79 @@ const AddResepPage = () => {
                 </button>
               </div>
             </div>
-            <div className="grid grid-cols-5 gap-4">
-              <div>
-                <label htmlFor="namaBahan">Bahan Baku</label>
-              </div>
-              <div>
-                <label htmlFor="jumlah">Jumlah</label>
-              </div>
-            </div>
-            <div>
-              {detailResep.map((val, i) => (
-                <div className="grid grid-cols-5 gap-4 mt-3">
-                  <div className="form-control">
-                    <select
-                      name="id_bahan_baku"
-                      id="namaBahan"
-                      className="select select-bordered bg-white"
-                      onChange={(event) => handleChange(i, event)}
-                    >
-                      <option disabled selected>
-                        Pilih Bahan Baku
-                      </option>
-                      {dataBahanBaku.map((bahan) => (
-                        <option value={bahan.id_bahan_baku}>
-                          {bahan.nama_bahan_baku}
-                        </option>
-                      ))}
-                    </select>
-                  </div>
-                  <div className="form-control">
-                    <input
-                      type="number"
-                      name="jumlah_bahan"
-                      id="jumlah"
-                      className="input input-bordered bg-white"
-                      onChange={(event) => handleChange(i, event)}
-                    />
-                  </div>
-                  <div>
-                    <button
-                      className="btn btn-error text-white"
-                      onClick={(event) => handleDelete(i, event)}
-                    >
-                      Delete
-                    </button>
+            <div className="divider m-1"></div>
+              <div className="mt-2">
+                <div className="form-control">
+                  <label htmlFor="namaResep">Nama Resep</label>
+                  <div className="grid grid-cols-2">
+                    <div>
+                      <input
+                        type="text"
+                        id="namaResep"
+                        name="nama_resep"
+                        className="input input-bordered bg-white"
+                        onChange={handleNamaResep}
+                      />
+                    </div>
                   </div>
                 </div>
-              ))}
+                <div className="grid grid-cols-5 gap-4">
+                  <div>
+                    <label htmlFor="namaBahan">Bahan Baku</label>
+                  </div>
+                  <div>
+                    <label htmlFor="jumlah">Jumlah</label>
+                  </div>
+                </div>
+                <div>
+                  {detailResep.map((val, i) => (
+                    <div key={i} className="grid grid-cols-5 gap-4 mt-3">
+                      <div className="form-control">
+                        <select
+                          name="id_bahan_baku"
+                          id="namaBahan"
+                          className="select select-bordered bg-white"
+                          defaultValue="Pilih Bahan Baku"
+                          onChange={(event) => handleChange(i, event)}
+                        >
+                          <option disabled>
+                            Pilih Bahan Baku
+                          </option>
+                          {dataBahanBaku.map((bahan) => (
+                            <option key={bahan.id_bahan_baku} value={bahan.id_bahan_baku}>
+                              {bahan.nama_bahan_baku}
+                            </option>
+                          ))}
+                        </select>
+                      </div>
+                      <div className="form-control">
+                        <input
+                          type="number"
+                          name="jumlah_bahan"
+                          id="jumlah"
+                          className="input input-bordered bg-white"
+                          onChange={(event) => handleChange(i, event)}
+                        />
+                      </div>
+                      <div>
+                        <button disabled={detailResep.length === 1}
+                          className="btn btn-error text-white"
+                          onClick={(event) => handleDelete(i, event)}
+                        >
+                          Delete
+                        </button>
+                      </div>
+                    </div>
+                  ))}
+                </div>
+              </div>
+            <div className="mt-3">
+              <button className="btn btn-success text-white" type="submit">
+                Submit
+              </button>
             </div>
-          </div>
-          <div className="mt-3">
-            <button className="btn btn-info" type="submit">
-              Submit
-            </button>
-          </div>
-        </form>
+          </form>
+        </div>
       </div>
     </div>
   );
